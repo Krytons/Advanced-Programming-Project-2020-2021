06537594a5c42f9e7bc41be18f046c6d5d8baf95
Bartolomeo_Caruso <48251346+Krytons@users.noreply.github.com>
2021-02-10 00:21:10 +0100

    Solved scheduler first start failure

--- Django/apl_api/__init__.py
+++ Django/apl_api/__init__.py
@@ -1,1 +1,13 @@
-default_app_config = 'apl_api.apps.AplApiConfig'
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+HOST = env.str('MONGO_HOST')
+PORT = env.int('MONGO_PORT')
+DATABASE = env.str('MONGO_DB_NAME')
+
+from pymongo import MongoClient
+connection = MongoClient(HOST, PORT) #Connect to mongodb
+db = connection[DATABASE]
+if 'django_apscheduler_djangojob' in db.list_collection_names():
+    default_app_config = 'apl_api.apps.AplApiConfig'
