cd14a9975b38136aa89953d2f06bfc04d2200bf7
Bartolomeo_Caruso <48251346+Krytons@users.noreply.github.com>
2021-02-10 16:12:38 +0100

    Added new classes and updated the old ones

--- Xamarin/XamarinFrontEnd/Classi/RequestObservation.cs
+++ Xamarin/XamarinFrontEnd/Classi/RequestObservation.cs
@@ -6,8 +6,16 @@ namespace XamarinFrontEnd.Classi
 {
     class RequestObservation
     {
+       
         public Product product { get; set; }
         public string threshold_price { get; set; }
         public string email { get; set; }
+
+        public RequestObservation(Product product, string threshold_price, string email)
+        {
+            this.product = product;
+            this.threshold_price = threshold_price;
+            this.email = email;
+        }
     }
 }
