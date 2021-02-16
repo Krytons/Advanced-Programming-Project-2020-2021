af03d6c104b200786086a984b26a5c286bbb06dc
Bartolomeo_Caruso <48251346+Krytons@users.noreply.github.com>
2021-02-16 15:43:12 +0100

    Removed comments

--- Xamarin/XamarinFrontEnd/UserObservations.xaml.cs
+++ Xamarin/XamarinFrontEnd/UserObservations.xaml.cs
@@ -27,36 +27,6 @@ namespace XamarinFrontEnd
         {
             CompleteObservations = await ObservationRequest.GetAllUserObservation();
             MyCollectionView.ItemsSource = CompleteObservations;
-            /*
-            int rowIndex = 0;
-            foreach(RequestObservation element in CompleteObservations)
-            {
-                var image_element = new Image
-                {
-                    Source = element.product.Gallery_url,
-                    Aspect = Aspect.AspectFill,
-                    HeightRequest = 130,
-                    WidthRequest = 130
-                };
-                GridLayout.Children.Add(image_element, 0, rowIndex);
-                Grid.SetRowSpan(image_element, 2);
-                var product_title_label = new Label
-                {
-                    Text = element.product.Title,
-                    FontAttributes = FontAttributes.Italic,
-                    VerticalOptions = LayoutOptions.End
-                };
-                GridLayout.Children.Add(product_title_label, 1, rowIndex);
-                var price_label = new Label
-                {
-                    Text = "Threshold: €" + element.threshold_price,
-                    FontAttributes = FontAttributes.Bold
-                };
-                GridLayout.Children.Add(price_label, 1, (rowIndex+1));
-
-                rowIndex = rowIndex + 2;
-            }
-            */
         }
 
         private void OnFavoriteSwipeItemInvoked(object sender, EventArgs e)
