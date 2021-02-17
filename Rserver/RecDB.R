4b1b301ab10f686fd4a428ec52cc2e441b7eb318
Gabriele Costanzo <g.costanzo.web@gmail.com>
2021-02-17 16:44:41 +0100

    :bug: Fix delle classi RecSys e RecDB
    :bug: Fix del funzionamento delle due classi, RecSys e RecDB
    :art: Adattate le classi al formato di S4 Class

--- Rserver/RecDB.R
+++ Rserver/RecDB.R
@@ -9,31 +9,69 @@ script.dir <- dirname(sys.frame(1)$ofile)
 #################################################
 
 # classe: "RecDB"
-setClass("RecDB", slots=list(currList="list", seqNum="numeric", reqs="list"))
+RecDB <- setClass(
+  "RecDB", 
+  
+  slots=c(
+    currList="list",
+    seqNum="numeric",
+    reqs="list"
+  ),
+  
+  prototype=list(
+    currList=list(),
+    seqNum=0,
+    reqs=list()
+  )
+)
 
 # costruttore
-RecDB <- function(l, seq){
-  rdb = new("RecDB", currList=list(), seqNum=seq, reqs=list())
-  
-  rdb <- update(rdb, l)
-  
-  return(rdb)
-}
+setMethod(
+  "initialize",
+  "RecDB",
+  function(.Object, l, seq){
+    .Object@seqNum <- seq
+    .Object <- update(.Object, l)
+    return(.Object)
+  }
+)
+
 
 # metodi
-update <- function(rdb, l){
-  filtList <- Filter(function(x){return(length(x)>0)}, l)
-  print(filtList)
-  
-  oldList <- rdb@currList
-  
-  removedList <- Map(function(x){return(character(0))}, oldList[!(oldList %in% filtList)])
-  newList <- filtList[!(filtList %in% oldList)]
-  changedList <- filtList[(filtList %in% oldList)]
-  changedList <- changedList[unlist(sapply(names(changedList), function(x) !identical(changedList[[x]], oldList[[x]])))]
-  
-  combinedList <- c(removedList, newList, changedList)
-  rdb@reqs[rdb@seqNum] <- toJSON(combinedList)
-  #rdb@seqNum <- rdb@seqNum + 1
-  return(rdb)
+hasGeneric=getGeneric("update")
+if(is.null(hasGeneric)){
+  setGeneric("update", function(r, l) standardGeneric("update"))
+}else{ 
+  setGeneric("update", hasGeneric)
 }
+setMethod("update", "RecDB",
+  function(r, l){
+    filtList <- Filter(function(x){return(length(x)>0)}, l)
+    
+    oldList <- r@currList
+    
+    removedList <- Map(function(x){return(character(0))}, oldList[!(oldList %in% filtList)])
+    newList <- filtList[!(filtList %in% oldList)]
+    changedList <- filtList[(filtList %in% oldList)]
+    changedList <- changedList[unlist(sapply(names(changedList), function(x) !identical(changedList[[x]], oldList[[x]])))]
+    
+    combinedList <- c(removedList, newList, changedList)
+
+    reqs <- r@reqs
+    index <- toString(r@seqNum)
+    reqs[index] <- toJSON(combinedList)
+    
+    r@reqs <- reqs
+    r@seqNum <- (r@seqNum + 1)
+    return(r)
+  }
+)
+
+
+setGeneric("increaseCounter", function(rdb) standardGeneric("increaseCounter"))
+setMethod("increaseCounter", "RecDB",
+  function(rdb){
+    rdb@seqNum <- (rdb@seqNum + 1)%%900
+    return(rdb)
+  }
+)
--- Rserver/RecommendationSystem.R
+++ Rserver/RecommendationSystem.R
@@ -6,79 +6,113 @@ lapply(list.of.packages, require, character.only = TRUE)
 
 #################################################
 # classe: "RecSys"
-setClass("RecSys", slots=list(m="data.frame", model="ANY", recs="ANY"))
+RecSys <- setClass(
+  "RecSys", 
+  
+  slots=c(
+    m = "data.frame",
+    model = "ANY",
+    recs = "ANY"
+    ),
+  
+  prototype=list(
+    m = data.frame(),
+    model = NULL,
+    recs = NULL
+  )
+)
 
 # costruttore
-RecSys <- function(json.data){
-  df = as.data.frame(json.data$data)
-  row.names(df) <- json.data$index
-  colnames(df)<- json.data$columns
-  
-  rs = new("RecSys", m=df, model=NULL, recs=NULL)
-  
-  rs <- removeEmptyEntries(rs)
-  rs <- train(rs)
-  
-  return(rs)
-}
+setMethod(
+  "initialize",
+  "RecSys",
+  function(.Object, json){
+    if(length(json)>0){
+      df = as.data.frame(json$data)
+      row.names(df) <- json$index
+      colnames(df)<- json$columns
+      
+      .Object@m <- df
+    }
+    if(length(.Object@m)>0) .Object <- train(removeEmptyEntries(.Object))
+    return(.Object)
+  }
+)
+
 
 # metodi
-train <- function(rs){
-  df <- rs@m
-  
-  binaryProductWatchList <- as(as.matrix(df), "binaryRatingMatrix")
-  model <- Recommender(data=binaryProductWatchList, method="UBCF", parameter=list(method="cosine"))
-  
-  num = min(c(10, ncol(df)))
-  
-  recommendations <- predict(model, binaryProductWatchList, n=num)
-  
-  rs@model <- model
-  rs@recs <- recommendations
-  return(rs)
-}
+setGeneric("train", function(rs) standardGeneric("train"))
+setMethod("train", "RecSys",
+  function(rs){
+    df <- rs@m
+    
+    binaryProductWatchList <- as(as.matrix(df), "binaryRatingMatrix")
+    model <- Recommender(data=binaryProductWatchList, method="UBCF", parameter=list(method="cosine"))
+    
+    num = min(c(10, ncol(df)))
+    
+    recommendations <- predict(model, binaryProductWatchList, n=num)
+    
+    rs@model <- model
+    rs@recs <- recommendations
+    return(rs)
+  }
+)
 
-removeEmptyEntries <- function(rs){
-  df <- rs@m
-  non.null.rows <- Filter(function(x){return(sum(df[x,])>0)}, rownames(df))
-  non.null.cols <- Filter(function(x){return(sum(df[,x])>0)}, names(df))
-  df2 <- df[non.null.rows,]
-  df3 <- df2[,non.null.cols]
-  rs@m <- df3
-  return(rs)
-}
 
-update <- function(rs, l){
-  df <- rs@m
-  for(x in l){
-    if(length(x)==3){
-      add <- x[[1]]
-      row <- x[[2]]
-      col <- x[[3]]
-      
-      print(c(add, row, col))
-      
-      if(add){
-        if(!(col %in% names(df))){ 
-          print(paste("Adding column", col, sep=" "))
-          df[,col] <- 0
+setGeneric("removeEmptyEntries", function(rs) standardGeneric("removeEmptyEntries"))
+setMethod("removeEmptyEntries", "RecSys",
+  function(rs){
+    df <- rs@m
+    non.null.rows <- Filter(function(x){return(sum(df[x,])>0)}, rownames(df))
+    non.null.cols <- Filter(function(x){return(sum(df[,x])>0)}, names(df))
+    df2 <- df[non.null.rows,]
+    df3 <- df2[,non.null.cols]
+    rs@m <- df3
+    return(rs)
+  }
+)
+
+
+hasGeneric=getGeneric("update")
+if(is.null(hasGeneric)){
+  setGeneric("update", function(r, l) standardGeneric("update"))
+}else{ 
+  setGeneric("update", hasGeneric)
+}
+setMethod("update", "RecSys",
+  function(r, l){
+    df <- r@m
+    for(x in l){
+      if(length(x)==3){
+        add <- x[[1]]
+        row <- x[[2]]
+        col <- x[[3]]
+        
+        #print(c(add, row, col))
+        
+        if(add){
+          if(!(col %in% names(df))){ 
+            print(paste("Adding column", col, sep=" "))
+            df[,col] <- 0
+          }
+          if(!(row %in% rownames(df))){
+            print(paste("Adding row", row, sep=" "))
+            df[row,] <- 0
+          }
+          df[row, col] <-  1
+          #print(df)
+        }else{
+          if(!is.null(df[row, col])) df[row, col] <- 0
         }
-        if(!(row %in% rownames(df))){
-          print(paste("Adding row", row, sep=" "))
-          df[row,] <- 0
-        }
-        df[row, col] <-  1
-        print(df)
-      }else{
-        if(!is.null(df[row, col])) df[row, col] <- 0
       }
     }
+    
+    r@m <- df
+    r <- removeEmptyEntries(r)
+    r <- train(r)
+    return(r)
   }
-  
-  rs@m <- df
-  rs <- removeEmptyEntries(rs)
-  rs <- train(rs)
-  return(rs)
-}
+)
 
 
